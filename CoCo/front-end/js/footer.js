b83d5e9917280f54581330ab031c8caee3b727be
trungtruc2357 <69811172+trungtruc2357@users.noreply.github.com>
2022-04-29 20:46:35 +0700

    fix footer homapage admin,manager,related-people to js

--- CoCo/front-end/js/footer.js
+++ CoCo/front-end/js/footer.js
@@ -18,13 +18,13 @@ function handleFooter() {
 
     let span1 = creElem('span','fa-solid fa-phone', []);
     span1.setAttribute('style','margin-right:5px');
-    let span2 = creElem('span','', [document.createTextNode('+84945335984')]);
+    let span2 = creElem('span','', [document.createTextNode('+84921360093')]);
 
     let contact1 = creElem('div','footer-contact',[span1,span2]);
 
     let span3 = creElem('span','fa-solid fa-envelope', []);
     span3.setAttribute('style','margin-right:5px');
-    let span4 = creElem('span','', [document.createTextNode('coco123@gmail.com')]);
+    let span4 = creElem('span','', [document.createTextNode('coco@gmail.com')]);
     let logo = creElem('img','', []);
     logo.setAttribute('src','/images/Logo.png');
     logo.setAttribute('alt','logo');
@@ -39,7 +39,7 @@ function handleFooter() {
     let footer_top = creElem('div','footer-top',[about_us,contact]);
 
     let hr = creElem('hr','',[]);
-    hr.setAttribute('style','border: 2px solid white;background-color: white; width: 98%');
+    hr.setAttribute('style','border: 2px solid white;background-color: white; width: 98%; margin:1%');
     
     let span5 = creElem('span','', [document.createTextNode('Cùng nhau vượt qua Đại dịch Covid-19')]);
     span5.setAttribute('style','float: left;');
