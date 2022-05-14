f500f148b61b795599fbc15357314f3e837660a2
trungtruc2357 <69811172+trungtruc2357@users.noreply.github.com>
2022-05-15 02:49:51 +0700

    update manager/related-people

--- CoCo/front-end/manager/related-people/js/handle.js
+++ CoCo/front-end/manager/related-people/js/handle.js
@@ -37,85 +37,40 @@ var contents = [
     {name:"Nguyễn Đăng Tiến Thành", identityID: "012345678910", yearOfBirth: "2001", status: "F0", treatmentPlace: "Quân khu 14, TP.Hồ Chí Minh"}
 ];
 
-if(typeof(Storage) !== 'undefined'){
-    if (localStorage.length === 0 || localStorage.getItem("listManager") === null){
-          localStorage.setItem('listManager', JSON.stringify(listManager));
-    } else {
-        listManager= JSON.parse(localStorage.getItem('listManager'));
-    }
-}
-loadListManagerTable();
-
-function createData(data, classData){
-    let a = document.createElement('td');
-    a.setAttribute("class", classData);
-    let textNode = document.createTextNode(data);
-    a.appendChild(textNode);
-    return a;
-}
-function createDataBySTT(STT){
-    let a = document.createElement('td');
-    a.setAttribute("class", "table-stt");
-    let textNode = document.createTextNode(STT);
-    a.appendChild(textNode);
-    return a;
-}
-function createButton(status){
-    let a = document.createElement('td');
-    let lock = document.createElement('span');
-    a.setAttribute("class", "table-lock");
-    if (status){
-        lock.setAttribute("class", "fa-solid fa-lock-open");
-    } else{
-        lock.setAttribute("class", "fa-solid fa-lock");
-    }
-    a.appendChild(lock);
-    return a;
-}
-
 function renderTable(){
     let headerRow = document.createElement('tr');
     keys.forEach(key=>{
         let textNode = document.createTextNode(headers[key]);
-        let th = creElem('th',[
-            {key:'class',value:'th-style'},
-            {key:'scope',value:'col'}
-        ],textNode);
-        //let th = document.createElement('th');
-        //th.setAttribute("class", "th-style");
-        //th.setAttribute("scope", "col");
-        //th.appendChild(textNode);
+        let th = creElem('th',{class:'th-style', scope:'col'},textNode);
         headerRow.appendChild(th);
     });
-    //let theader = document.createElement('thead');
+    
     let theader = creElem("thead",'',headerRow);
-    //theader.appendChild(headerRow);
-    //let table = document.createElement('table');
-    //table.setAttribute("class", "table table-striped");
-    //table.appendChild(theader);
-    let table = creElem("table","table table-striped",theader);
+
     let tbody = document.createElement('tbody');
-    let id = 1;
+    let id = 0;
     contents.forEach(content=>{
-        let dataRow = creElem('tr','',[]);
-        dataRow.setAttribute("data-href",'/manager/related-people/detail-info');
-        //let dataRow = document.createElement('tr');
-        //dataRow.setAttribute("data-href","/manager/related-people/detail-info");
-        let STT = createDataBySTT(id.toString());
-        STT.onclick=function(){
-            window.location.href="./manager-details-page.html";
-        }
-        let name = createData(content["name"], "table-name");
-        name.onclick=function(){
-            window.location.href="./manager-details-page.html";
-        }
-        let button = createButton(content["status"]);
-        dataRow.appendChild(STT);
-        dataRow.appendChild(name);
-        dataRow.appendChild(button);
-        tbody.appendChild(dataRow);
         ++id;
+        let dataRow = creElem('tr','',[]);
+        dataRow.setAttribute("data-href",'/manager/related-people/detail-info',[]);
+        
+        keys.forEach(key=>{
+            let info = id.toString();
+            if (key !== "id") {
+                info = content[key];
+            }
+            
+            dataRow.appendChild(creElem('td',"table-name",document.createTextNode(info)));
+        });
+
+        tbody.appendChild(dataRow);
     });
-    table.appendChild(tbody);
+    
+    let table = creElem("table","table table-striped",[theader,tbody]);
+    
+    let myTable = document.querySelector("#table");
+    
     myTable.appendChild(table);
 }
+
+renderTable();
