853e1df8fc42c541329e9e92c26fda82fc6692cf
Nguyễn Lê Bảo Thi <thinguyenlebao@gmail.com>
2022-07-02 21:44:41 +0700

    change admin html to hbs

--- CoCo/index.js
+++ CoCo/index.js
@@ -1,26 +1,46 @@
-const express = require('express');
+const express = require('express')
+const path = require('path')
+const expressHbs = require('express-handlebars');
+const bodyParser = require('body-parser')
 const cors = require("cors");
-const route = require('./routes');
+const route = require('./routes/index');
 const db = require('./database');
 const app = express();
-const port = 8000;
+ const port = 8000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//app.listen(5555)
 
-// app.engine("html", require("ejs").renderFile);
-// app.set('views', './src/views')
+
+const hbs = expressHbs.create({
+    layoutsDir: path.join(__dirname, 'views/layouts'),
+    partialsDir: path.join(__dirname, 'views/partials'),
+    extname: 'hbs',
+    defaultLayout: 'layout'
+})
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 const corsOptions = {
-  origin: '*',
-  credentials: true,       //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-}
+    origin: '*',
+    credentials: true,       //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  }
 app.use(cors(corsOptions)) // Use this after the variable declaration
 
-route(app);
+// route(app);
 db.connect();
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+app.get('/admin', (req, res) => {
+    res.render('admin/index')
+})
+
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), ()=>{
+    console.log(`server is running on port ${app.get('port')}`);
 });
