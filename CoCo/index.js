e2a5a9ef644386de703b9c92354a5d53534e5e3c
Nguyễn Lê Bảo Thi <thinguyenlebao@gmail.com>
2022-07-02 23:07:25 +0700

    add AdminController

--- CoCo/index.js
+++ CoCo/index.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const path = require('path')
 const expressHbs = require('express-handlebars');
@@ -9,11 +8,37 @@ const bodyParser = require('body-parser')
 const app = express();
 const port = 8000;
 
+role = "guest"
+
 const hbs = expressHbs.create({
   layoutsDir: path.join(__dirname, 'views/layouts'),
   partialsDir: path.join(__dirname, 'views/partials'),
   extname: 'hbs',
-  defaultLayout: 'layout'
+  defaultLayout: 'layout',
+
+  // create custom helpers
+  helpers:{
+    isGuest: function(options) {
+      if(role === "guest")
+        return options.fn(this);
+      return options.inverse(this);
+    },
+    isAdmin: function(options) {
+      if(role ===	'admin')
+        return options.fn(this);
+      return options.inverse(this);
+    },
+    isManager: function(options) {
+      if(role ===	'manager')
+        return options.fn(this);
+      return options.inverse(this);
+    },
+    isRelatedPeople: function(options) {
+      if(role === 'related people')
+        return options.fn(this);
+      return options.inverse(this);
+    }
+  }
 });
 
 app.engine('hbs', hbs.engine);
