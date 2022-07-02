1dedd7bbda92f708f8f6ca9193cdc1ba651da299
Nguyễn Lê Bảo Thi <thinguyenlebao@gmail.com>
2022-07-02 21:54:55 +0700

    change index.js

--- CoCo/index.js
+++ CoCo/index.js
@@ -3,9 +3,9 @@ const express = require('express')
 const path = require('path')
 const expressHbs = require('express-handlebars');
 const bodyParser = require('body-parser')
-const cors = require("cors");
-const route = require('./routes/index');
-const db = require('./database');
+//const cors = require("cors");
+// const route = require('./routes/index');
+// const db = require('./database');
 const app = express();
 const port = 8000;
 
@@ -25,19 +25,15 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-const corsOptions = {
-    origin: '*',
-    credentials: true,       //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-  }
-app.use(cors(corsOptions)) // Use this after the variable declaration
+// const corsOptions = {
+//     origin: '*',
+//     credentials: true,       //access-control-allow-credentials:true
+//     optionSuccessStatus: 200,
+//   }
+// app.use(cors(corsOptions)) // Use this after the variable declaration
 
 // route(app);
-db.connect();
-
-app.get('/admin', (req, res) => {
-    res.render('admin/index')
-})
+// db.connect();
 
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), ()=>{
