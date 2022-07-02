783c0fd2849c316ff0ca3f7b23a1f94718f02462
trungtruc2357 <69811172+trungtruc2357@users.noreply.github.com>
2022-07-02 23:35:29 +0700

    nothing

--- CoCo/index.js
+++ CoCo/index.js
@@ -63,5 +63,5 @@ app.use(bodyParser.json())
 
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), ()=>{
-    console.log(`server is running on port ${app.get('port')}`);
+    console.log(`server is running on http://localhost:${app.get('port')}`);
 });
