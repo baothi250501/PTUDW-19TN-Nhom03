44b0a08cd3dc3153bc84914119dd0d09b8100fdb
nghiaump <nghiaump@gmail.com>
2022-07-04 00:36:28 +0700

    update the schema of ManagedHistory

--- CoCo/models/related-people/ManagedHistory.js
+++ CoCo/models/related-people/ManagedHistory.js
@@ -6,30 +6,30 @@ const ManagedHistory = mongoose.Schema(
       type: String,
       required: true,
     },
-    managements: [
-      {
-        updateType: {
-          type: Number,
-          required: true,
-        },
-        infectionStatus: {
-          type: String,
-          required: true,
-        },
-        place: {
-          type: String,
-          required: true,
-        },
-        note: {
-          type: String,
-          required: false,
-        },
-        dateTime: {
-          type: Date,
-          required: true,
-        }
-      },
-    ],
+    updateType: {
+      type: Number,
+      required: true,
+    },
+    infectionStatus: {
+      type: String,
+      required: true,
+    },
+    place: {
+      type: String,
+      required: true,
+    },
+    note: {
+      type: String,
+      required: false,
+    },
+    dateTime: {
+      type: Date,
+      required: true,
+    },
+    managerUsername: {
+      type: String,
+      require: true
+    }
   }
 );
 
