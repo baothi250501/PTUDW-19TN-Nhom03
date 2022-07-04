705132c058f859bbb3e9309f5053661cdabff7ff
anniele09 <lephamlananh1709@gmail.com>
2022-07-04 18:21:17 +0700

    fix edit products

--- CoCo/controllers/manager/PackageController.js
+++ CoCo/controllers/manager/PackageController.js
@@ -1,4 +1,7 @@
-const packages = [
+const Package = require('../../models/manager/Package.js');
+const Product = require('../../models/manager/Product.js');
+var packages = []
+/*const packages = [
     {stt: "1", name:"Combo Thịt, Cá, Trứng 1", price:"200.000"},
     {stt: "2", name:"Combo Thịt, Cá, Trứng 2", price:"250.000"},
     {stt: "3", name:"Combo Thịt, Cá, Trứng 3", price:"300.000"}
@@ -12,10 +15,37 @@ const _package = {
     packageNum: 10,
     timeLimit: 10,
     
-};
+};*/
 class PackageController {
     index(req, res) {
-        res.render('manager/products/package-list-page', {packages});
+        Package.find({}).then(function(doc) {
+            packages = doc;
+            let stt = 1;
+            packages.forEach((item) => {
+                item.stringId = item._id.toString();
+                let price = 0;
+                item.productInfo = [];
+                item.price = 0;
+                item.products.forEach((id)=>{
+                    var tmpInfo = {};
+                    Product.findById(id, function(err, pro){
+                        if(err) throw err;
+                        tmpInfo.name = pro.name;
+                        tmpInfo.price = pro.price;
+                        tmpInfo.stringId = id;
+                        item.price += pro.price;
+                        item.productInfo.push(tmpInfo);
+                    });
+                    
+                });
+                item.price = "" + price;
+                item.stt = "" + stt;
+                stt++;
+            });
+            
+            res.render('manager/products/package-list-page', {packages});
+        });
+        
     }
     add(req, res){
         res.render('manager/products/add-package-page', {_package});
