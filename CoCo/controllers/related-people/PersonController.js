5031c9cef6888b6ef2f3604226def0095642c0ad
nghiaump <nghiaump@gmail.com>
2022-07-04 15:33:56 +0700

    update async functions of PersonController

--- CoCo/controllers/related-people/PersonController.js
+++ CoCo/controllers/related-people/PersonController.js
@@ -4,9 +4,9 @@ const ConsumptionHistory = require('../../models/related-people/ConsumptionHisto
 const PaymentHistory = require('../../models/related-people/PaymentHistory.js');
 const Package = require('../../models/manager/Package.js');
 class PersonController {
-  index(req, res) {
-    const username = req.body.username;
-    const person = Person.findOne({username : username}).exec();
+  async index(req, res) {
+    const username = req.query.username;
+    const person = await Person.findOne({username : username}).exec();
     if(person){
       res.render("related-people/information", { person });
     }
@@ -14,16 +14,16 @@ class PersonController {
       res.status(404).send({
         "error": {
             "code": 404,
-            "message": " User Not Found"
+            "message": "User Not Found"
         }
     });
     }
   }
 
-  managedHistory(req, res) {
-    const username = req.body.username;
-    const person = Person.findOne({username : username}).exec();
-    const histories = ManagedHistory.find({username : username}).sort({dateTime : 1}).exec();
+  async managedHistory(req, res) {
+    const username = req.query.username;
+    const person = await Person.findOne({username : username}).exec();
+    const histories = await ManagedHistory.find({username : username}).sort({dateTime : 1}).exec();
     if(person){
       res.render("related-people/managed-history", { person, histories});
     }
@@ -31,17 +31,17 @@ class PersonController {
       res.status(404).send({
         "error": {
             "code": 404,
-            "message": " User Not Found"
+            "message": "User Not Found"
         }
     });
     }
   }
 
-  consumptionHistory(req, res) {
-    const username = req.body.username;
-    const person = Person.findOne({username : username}).exec();
+  async consumptionHistory(req, res) {
+    const username = req.query.username;
+    const person = await Person.findOne({username : username}).exec();
 
-    const consumption = ConsumptionHistory.find({username : username}).sort({dateTime : 1}).exec();
+    const consumption = await ConsumptionHistory.find({username : username}).sort({dateTime : 1}).exec();
     if(person){
       res.render("related-people/consumption-history", { person, consumption });
     }
@@ -55,10 +55,10 @@ class PersonController {
     }
   }
 
-  paymentHistory(req, res) {
-    const username = req.body.username;
-    const person = Person.findOne({username : username}).exec();
-    const payment = PaymentHistory.find({username : username}).sort({dateTime : 1}).exec();
+  async paymentHistory(req, res) {
+    const username = req.query.username;
+    const person = await Person.findOne({username : username}).exec();
+    const payment = await PaymentHistory.find({username : username}).sort({dateTime : 1}).exec();
     if(person){
       res.render("related-people/consumption-history", { person, payment });
     }
@@ -66,15 +66,15 @@ class PersonController {
       res.status(404).send({
         "error": {
             "code": 404,
-            "message": " User Not Found"
+            "message": "User Not Found"
         }
     });
     }
 
   }
 
-  packageList(req, res) {
-    const allPackages = Package.find({});
+  async packageList(req, res) {
+    const allPackages = await Package.find({});
     res.render("related-people/package-list", {allPackages});
   }
 
